6f10a9deff5c0ee71ce1a4d31bd30f64232171ab
nvinayshetty <nvinayshetty@gmail.com>
2019-03-24 10:53:48 +0700

    state machine parser

---
+++ src/main/kotlin/dev/vinayshetty/stateart/parser/StateMachineParser.kt
@@ -0,0 +1,84 @@
+package dev.vinayshetty.stateart.parser
+
+import dev.vinayshetty.stateart.builder.StateMachineBuilder
+import dev.vinayshetty.stateart.datastructure.StateMachine
+import dev.vinayshetty.stateart.lexer.StateMachineTokenizer
+
+class StateMachineParser(private val stateMachineBuilder: StateMachineBuilder) : StateMachineTokenizer {
+    private var previousEvent: ParserEvents? = null
+
+    override fun state(state: String) {
+        handleParserEvent(ParserEvents.State(state))
+    }
+
+    override fun event(event: String) {
+        handleParserEvent(ParserEvents.Event(event))
+    }
+
+    override fun transition(nextState: String) {
+        handleParserEvent(ParserEvents.Transition(nextState))
+    }
+
+    override fun doNotTransition() {
+        handleParserEvent(ParserEvents.NoTransition)
+    }
+
+    override fun stateMachine(): StateMachine {
+        return stateMachineBuilder.build()
+    }
+
+    private fun handleParserEvent(currentEvent: ParserEvents) {
+        if (previousEvent == null && currentEvent is ParserEvents.State) {
+            stateMachineBuilder.addState(currentEvent.name)
+        } else if (previousEvent != null) {
+            when (currentEvent) {
+                is ParserEvents.State -> {
+                    when (previousEvent) {
+                        is ParserEvents.Transition,
+                        is ParserEvents.NoTransition -> stateMachineBuilder.addState(currentEvent.name)
+                        else -> {
+                            val message =
+                                "Malformed StateMachine:Event/state ${currentEvent.name} can't appear after the  ${previousEvent.toString()}:"
+                            throw StateMachineParserException(message)
+                        }
+                    }
+                }
+                is ParserEvents.Event -> {
+                    when (previousEvent) {
+                        is ParserEvents.State,
+                        is ParserEvents.Transition,
+                        is ParserEvents.NoTransition -> stateMachineBuilder.addEvent(currentEvent.event)
+                        else -> {
+                            val message =
+                                """An eventName should't appear right after previous event,add transition for event ${currentEvent.event}
+                                hint:${currentEvent.event} appears after $previousEvent
+                            """
+                            throw StateMachineParserException(message)
+                        }
+                    }
+                }
+                is ParserEvents.Transition -> {
+                    when (previousEvent) {
+                        is ParserEvents.Event -> stateMachineBuilder.addTransitionToState(currentEvent.nextState)
+                        else -> {
+                            val message =
+                                "A transition can appear only after an event hint:$previousEvent appears after $currentEvent"
+                            throw StateMachineParserException(message)
+                        }
+                    }
+                }
+                is ParserEvents.NoTransition -> {
+                    when (previousEvent) {
+                        is ParserEvents.Event -> stateMachineBuilder.addNoTransition()
+                        else -> {
+                            val message =
+                                "doNotTransition can appear only after an event hint:dontTransition appears after ${previousEvent.toString()}"
+                            throw StateMachineParserException(message)
+                        }
+                    }
+                }
+            }
+        }
+        previousEvent = currentEvent
+    }
+}
---
+++ src/test/kotlin/dev/vinayshetty/stateart/parser/StateMachineParserTest.kt
@@ -0,0 +1,183 @@
+package dev.vinayshetty.stateart.parser
+
+import dev.vinayshetty.stateart.builder.StateMachineBuilder
+import org.hamcrest.CoreMatchers
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.Mockito.verify
+import org.mockito.MockitoAnnotations
+
+
+internal class StateMachineParserTest {
+
+    private lateinit var stateMachineParser: StateMachineParser
+
+    @Mock
+    private lateinit var stateMachineBuilder: StateMachineBuilder
+
+    @BeforeEach
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        stateMachineParser = StateMachineParser(stateMachineBuilder)
+    }
+
+    @Test
+    fun `add initial state`() {
+        stateMachineParser.state("liquid")
+        verify(stateMachineBuilder).addState("liquid")
+    }
+
+    @Test
+    fun `addState event after valid transitions`() {
+        val orderVerifier = Mockito.inOrder(stateMachineBuilder)
+
+        stateMachineParser.state("liquid")
+        stateMachineParser.event("vaporize")
+        stateMachineParser.transition("gas")
+        stateMachineParser.state("gas")
+        stateMachineParser.event("boil")
+        stateMachineParser.doNotTransition()
+
+        orderVerifier.verify(stateMachineBuilder).addState("liquid")
+        orderVerifier.verify(stateMachineBuilder).addEvent("vaporize")
+        orderVerifier.verify(stateMachineBuilder).addTransitionToState("gas")
+        orderVerifier.verify(stateMachineBuilder).addState("gas")
+        orderVerifier.verify(stateMachineBuilder).addEvent("boil")
+        orderVerifier.verify(stateMachineBuilder).addNoTransition()
+    }
+
+    @Test
+    fun `calling addState after an event should throw exception`() {
+        val exception = assertThrows<StateMachineParserException> {
+            stateMachineParser.state("liquid")
+            stateMachineParser.event("vaporize")
+            stateMachineParser.state("gas")
+        }
+        assertThat(
+            exception.message,
+            equalTo("Malformed StateMachine:Event/state gas can't appear after the  Event(event=vaporize):")
+        )
+    }
+
+    @Test
+    fun `state event  after previous State event should throw exception`() {
+        val exception = assertThrows<StateMachineParserException> {
+            stateMachineParser.state("liquid")
+            stateMachineParser.state("gas")
+        }
+        assertThat(
+            exception.message,
+            CoreMatchers.equalTo("Malformed StateMachine:Event/state gas can't appear after the  State(name=liquid):")
+        )
+    }
+
+    @Test
+    fun `an Event can occur after previous  State, Transition or NoTransition`() {
+        val orderVerifier = Mockito.inOrder(stateMachineBuilder)
+        stateMachineParser.state("liquid")
+        stateMachineParser.event("vaporize")
+
+        stateMachineParser.transition("gas")
+        stateMachineParser.event("vaporize")
+
+        stateMachineParser.doNotTransition()
+        stateMachineParser.event("boil")
+
+        orderVerifier.verify(stateMachineBuilder).addState("liquid")
+        orderVerifier.verify(stateMachineBuilder).addEvent("vaporize")
+        orderVerifier.verify(stateMachineBuilder).addTransitionToState("gas")
+        orderVerifier.verify(stateMachineBuilder).addEvent("vaporize")
+        orderVerifier.verify(stateMachineBuilder).addNoTransition()
+        orderVerifier.verify(stateMachineBuilder).addEvent("boil")
+    }
+
+    @Test
+    fun `a Transition can occur only after an event`() {
+        val orderVerifier = Mockito.inOrder(stateMachineBuilder)
+        stateMachineParser.state("liquid")
+        stateMachineParser.event("vaporize")
+        stateMachineParser.transition("gas")
+        orderVerifier.verify(stateMachineBuilder).addState("liquid")
+        orderVerifier.verify(stateMachineBuilder).addEvent("vaporize")
+        orderVerifier.verify(stateMachineBuilder).addTransitionToState("gas")
+    }
+
+
+    @Test
+    fun `a Transition after a State throws exception `() {
+        val exception = assertThrows<StateMachineParserException> {
+            stateMachineParser.state("liquid")
+            stateMachineParser.transition("gas")
+        }
+        assertThat(
+            exception.message,
+            equalTo("A transition can appear only after an event hint:State(name=liquid) appears after Transition(nextState=gas)")
+        )
+    }
+
+    @Test
+    fun `a Transition after previous transition throws exception `() {
+        val exception = assertThrows<StateMachineParserException> {
+            stateMachineParser.state("liquid")
+            stateMachineParser.event("vaporize")
+            stateMachineParser.transition("gas")
+            stateMachineParser.transition("liquid")
+        }
+        assertThat(
+            exception.message,
+            equalTo("A transition can appear only after an event hint:Transition(nextState=gas) appears after Transition(nextState=liquid)")
+        )
+    }
+
+
+    @Test
+    fun `A Transition after previous doNotTransition throws exception `() {
+        val exception = assertThrows<StateMachineParserException> {
+            stateMachineParser.state("liquid")
+            stateMachineParser.event("vaporize")
+            stateMachineParser.doNotTransition()
+            stateMachineParser.transition("gas")
+        }
+    }
+
+    @Test
+    fun `a DoNotTransition can occur only after an event`() {
+        val orderVerifier = Mockito.inOrder(stateMachineBuilder)
+        stateMachineParser.state("liquid")
+        stateMachineParser.event("warm")
+        stateMachineParser.doNotTransition()
+        orderVerifier.verify(stateMachineBuilder).addState("liquid")
+        orderVerifier.verify(stateMachineBuilder).addEvent("warm")
+        orderVerifier.verify(stateMachineBuilder).addNoTransition()
+    }
+
+    @Test
+    fun `a DoNotTransition after a State throws exception `() {
+        assertThrows<StateMachineParserException> {
+            stateMachineParser.state("liquid")
+            stateMachineParser.doNotTransition()
+        }
+    }
+
+    @Test
+    fun `a DoNotTransition after previous DoNotTransition throws exception `() {
+        assertThrows<StateMachineParserException> {
+            stateMachineParser.state("liquid")
+            stateMachineParser.event("vaporize")
+            stateMachineParser.doNotTransition()
+            stateMachineParser.doNotTransition()
+        }
+    }
+
+
+    @Test
+    fun stateMachine() {
+        val stateMachine = stateMachineParser.stateMachine()
+        verify(stateMachineBuilder).build()
+    }
+}

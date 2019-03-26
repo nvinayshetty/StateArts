b47aed07d1faaba36b75295d4fecc9c7985e855f
nvinayshetty <nvinayshetty@gmail.com>
2019-03-26 22:41:04 +0700

    generate state and art

--- src/main/kotlin/dev/vinayshetty/stateart/intellij/StateArtLineMarker.kt
+++ src/main/kotlin/dev/vinayshetty/stateart/intellij/StateArtLineMarker.kt
@@ -5,15 +5,21 @@ import com.intellij.codeInsight.daemon.GutterIconNavigationHandler
 import com.intellij.codeInsight.daemon.LineMarkerInfo
 import com.intellij.codeInsight.daemon.LineMarkerProvider
 import com.intellij.openapi.editor.markup.GutterIconRenderer
+import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.util.IconLoader
+import com.intellij.openapi.vfs.LocalFileSystem
 import com.intellij.psi.PsiElement
 import com.intellij.psi.impl.source.tree.LeafPsiElement
 import com.intellij.util.Function
+import dev.vinayshetty.stateart.dotsbuilder.DotsBuilderImpl
+import dev.vinayshetty.stateart.lexer.StateMachineLexerImpl
 import dev.vinayshetty.stateart.parser.StateMachineParser
 import dev.vinayshetty.stateart.statemachinebuilder.StateMachineBuilderImpl
 import org.jetbrains.kotlin.lexer.KtTokens
 import org.jetbrains.kotlin.psi.KtProperty
 import java.awt.event.MouseEvent
+import java.io.File
+
 
 class StateArtLineMarker : LineMarkerProvider {
 
@@ -38,8 +44,32 @@ class StateArtLineMarker : LineMarkerProvider {
         psiElement?.let {
             val stateMachineText = psiElement.parent.text
             val stateMachineName = psiElement.text
+            val project = psiElement.project
+            val buildFolderPath = project.basePath + "/build"
             val stateMachineBuilder = StateMachineBuilderImpl()
             val stateMachineTokenizer = StateMachineParser(stateMachineBuilder)
+            val stateMachineLexer = StateMachineLexerImpl(stateMachineTokenizer)
+            val stateMachine = stateMachineLexer.lex(stateMachineText)
+            val stateMachineDotString = DotsBuilderImpl().build(stateMachineName, stateMachine)
+            val buildFolder = File(buildFolderPath)
+            if (buildFolder.exists() && buildFolder.isDirectory) {
+                val basepath = "$buildFolderPath/stateArt"
+                val statesFolderPath = "$basepath/states"
+                val artsFolderPath = "$basepath/arts"
+                File(statesFolderPath).mkdirs()
+                File(artsFolderPath).mkdirs()
+                File("$statesFolderPath/$stateMachineName.dot").printWriter().use { file ->
+                    file.print(stateMachineDotString)
+                    Runtime.getRuntime()
+                        .exec("dot -Tpng $statesFolderPath/$stateMachineName.dot -o $artsFolderPath/$stateMachineName.png")
+                    LocalFileSystem.getInstance().refresh(true)
+                    val path =
+                        LocalFileSystem.getInstance().refreshAndFindFileByPath("$artsFolderPath/$stateMachineName.png")
+                    path?.let {
+                        FileEditorManager.getInstance(project).openFile(it, true, true)
+                    }
+                }
+            }
         }
     }
 
